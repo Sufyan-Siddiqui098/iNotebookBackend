42fa7a623bd7060861b942179666191ed321928d
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-04 16:31:35 +0500

    listen is called inside to avoid issue in cyclick backend hosting

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ const mongoURI = process.env.MONGO_URI;
 // console.log(mongoURI)
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,console.log("connected Successfully !!"))
+    app.listen(port, () => {
+        console.log(`iNotebook app listening on port ${port}`)
+      })
 }
 
 module.exports =connectToMongo;
