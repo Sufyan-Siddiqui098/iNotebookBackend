20fa49ef2a3e56df98963c0735106b328c87b777
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-07-04 16:40:33 +0500

    reset

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-// app.listen(port, () => {
-//   console.log(`iNotebook app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`iNotebook app listening on port ${port}`)
+})
